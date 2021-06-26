58b372cd735c70b6b0e507716b153942b9ce45c2
DuvCabrera <eduardoviniciuscabrera@gmail.com>
2021-06-26 00:28:57 -0300

    add comments on the functions

--- basic-sintax/calc.kt
+++ basic-sintax/calc.kt
@@ -19,19 +19,19 @@ fun calculate(numberOne:Float, numberTwo:Float, opNumber:Int):Float{
         opNumber == 4 -> div(numberOne, numberTwo)
         else -> "Error: parameter does not match with the expected"
     }
-    """
+    /*
     Will receive 2 paremeters and one function
     :parameter numberOne : Float its a number
     :parameter numberTwo : Float its a number
     :parameter opNumber : Int its the number of the operation that will
     be executed 
-    """
+    */
 }
 
-fun sum(n1:Float, n2:Float) = n1.plus(n2) """ perform a sum between 2 parameters """
+fun sum(n1:Float, n2:Float) = n1.plus(n2) /* perform a sum between 2 parameters */
 
-fun minus(n1:Float, n2:Float) = n1.minus(n2) """ perform a 'minus' operation between 2 parameters """
+fun minus(n1:Float, n2:Float) = n1.minus(n2) /* perform a 'minus' operation between 2 parameters */
 
-fun times(n1:Float, n2:Float) = n1.times(n2) """ perform times operation between 2 parameters """
+fun times(n1:Float, n2:Float) = n1.times(n2) /* perform times operation between 2 parameters */
 
-fun div(n1:Float, n2:Float) = n1.div(n2) """ perform div between 2 parameters """
+fun div(n1:Float, n2:Float) = n1.div(n2) /* perform div between 2 parameters */
