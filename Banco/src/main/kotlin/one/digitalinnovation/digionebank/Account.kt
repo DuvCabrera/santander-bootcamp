5c225bac5db4a6ab7f86e9f1c2d2941a64d8f35f
DuvCabrera <eduardoviniciuscabrera@gmail.com>
2021-06-29 21:29:12 -0300

    add Account file

---
+++ Banco/src/main/kotlin/one/digitalinnovation/digionebank/Account.kt
@@ -0,0 +1,17 @@
+package one.digitalinnovation.digionebank
+
+import java.math.BigDecimal
+
+class Account {
+    val agencia: String,
+    val numero: String,
+    val Saldo: BigDecimal
+
+    fun deposito(valor: BigDecimal){
+
+    }
+
+    fun saque(valor: BigDecimal){
+
+    }
+}
