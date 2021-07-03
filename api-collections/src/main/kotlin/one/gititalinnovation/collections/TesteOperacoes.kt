48601ef9a9138819ddbcc976a61f31b5e6ab25f2
DuvCabrera <eduardoviniciuscabrera@gmail.com>
2021-07-03 12:19:58 -0300

    add new operations in testeoperacoes file

--- api-collections/src/main/kotlin/one/gititalinnovation/collections/TesteOperacoes.kt
+++ api-collections/src/main/kotlin/one/gititalinnovation/collections/TesteOperacoes.kt
@@ -15,8 +15,21 @@ fun main() {
     println("Media salarial: ${salarios.average()}")
     /* get the average of numbers in salario array */
 
-    val salariosMaiorQue2500 = salarios.filter{ it > 2500.0}
+    val salariosMaiorQue2500 = salarios.filter { it > 2500.0}
     /* get values bigger them 2500.00 and add to a new array*/
     println("--------------")
     salariosMaiorQue2500.forEach{print(it)}
+
+    println("--------------")
+    println(salarios.count { it in 2000.0..5000.0})
+    /* in a specific range count the number of values
+    * in that range that are in the array*/
+
+    println("--------------")
+    println(salarios.find { it == 1500.0})
+    /*look in the array and find the value*/
+
+    println("--------------")
+    println(salarios.any { it == 1000.0})
+    /*return a boolean if its found or not the element searched*/
 }
