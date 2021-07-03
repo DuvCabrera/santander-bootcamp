df0acaa948f222a97c39fca10af7663fb604a256
DuvCabrera <eduardoviniciuscabrera@gmail.com>
2021-07-03 11:15:05 -0300

    TestinArrayOf has been modified

--- api-collections/src/main/kotlin/one/gititalinnovation/collections/TestintArrayOf.kt
+++ api-collections/src/main/kotlin/one/gititalinnovation/collections/TestintArrayOf.kt
@@ -1,5 +1,15 @@
 package one.gititalinnovation.collections
 
 fun main() {
-    val values =
+    val values = intArrayOf(2, 3, 4, 1, 10, 7)
+
+    values.forEach {
+        println(it)
+    }
+
+    println("-----------------------")
+    values.sort()
+    values.forEach {
+        println(it)
+    }
 }
