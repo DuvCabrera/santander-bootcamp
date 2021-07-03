9deadc9e4fc61ec07c91bd5406600805701b50c1
DuvCabrera <eduardoviniciuscabrera@gmail.com>
2021-07-03 12:57:34 -0300

    modified testelist file

--- api-collections/src/main/kotlin/one/gititalinnovation/collections/TesteList.kt
+++ api-collections/src/main/kotlin/one/gititalinnovation/collections/TesteList.kt
@@ -2,9 +2,9 @@ package one.gititalinnovation.collections
 
 /*class for test listOf function */
 fun main() {
-    val joao = Funcioario("João", 4000.0)
-    val pedro = Funcioario("Pedro", 2000.0)
-    val maria = Funcioario("Maria", 3000.0)
+    val joao = Funcioario("João", 4000.0, "CLT")
+    val pedro = Funcioario("Pedro", 2000.0, "PJ")
+    val maria = Funcioario("Maria", 3000.0, "CLT")
 
     /* array created with funcionario objects*/
     val funcionarios = listOf(joao, pedro, maria)
@@ -15,17 +15,29 @@ fun main() {
 
     /*sort the components in an array by a specific value*/
     println("---------------")
-    funcionarios.sortedBy { it.salario }.forEach {println(it)}
+    funcionarios
+        .sortedBy { it.salario }
+        .forEach {println(it)}
+
+    /*group objects in a list using an attribute as reference */
+    println("---------------")
+    funcionarios
+        .groupBy { it.tipoContratacao }
+        .forEach {println(it)}
+
+
 }
 
 /*funcionario class created and string appearance modified for better look*/
 data class Funcioario(
     val nome: String,
-    val salario: Double
+    val salario: Double,
+    val tipoContratacao: String
 ){
     override fun toString(): String =
         """
             Nome : $nome
             Salário: $salario
+            Tipo: $tipoContratacao
         """.trimIndent()
 }
