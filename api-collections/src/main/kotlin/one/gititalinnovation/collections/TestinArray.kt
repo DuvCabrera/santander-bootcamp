89422f4bb134e993b15a237302868832bc6aeec7
DuvCabrera <eduardoviniciuscabrera@gmail.com>
2021-07-03 10:44:47 -0300

    modified BootCalc and api-colections folders, on api was modified TestInArray file - add tests and descriptions

--- api-collections/src/main/kotlin/one/gititalinnovation/collections/TestinArray.kt
+++ api-collections/src/main/kotlin/one/gititalinnovation/collections/TestinArray.kt
@@ -8,4 +8,30 @@ fun main(){
     values[2] = 6
     values[3] = 3
     values[4] = 2
+
+    // for its utilized for access the values on "values" array
+    // then print
+    for (valor in values) {
+        println(valor)
+    }
+
+    println("--------------------")
+    //through lambda "forEach" print the valor on values
+    values.forEach { valor ->
+        println(valor)
+    }
+
+    println("---------------------")
+    // print the values in values through of the
+    // index on values
+    for (index in values.indices){
+        println(values[index])
+    }
+
+    println("---------------------")
+    // pre-orden the array components and print
+    values.sort()
+    for (valor in values) {
+        println(valor)
+    }
 }
