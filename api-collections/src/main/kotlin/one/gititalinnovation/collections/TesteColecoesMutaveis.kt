429dfcc35c489e6c5c0324740cbae7645783bf69
DuvCabrera <eduardoviniciuscabrera@gmail.com>
2021-07-03 19:37:26 -0300

    criated test class for mutable lists

---
+++ api-collections/src/main/kotlin/one/gititalinnovation/collections/TesteColecoesMutaveis.kt
@@ -0,0 +1,32 @@
+package one.gititalinnovation.collections
+
+fun main() {
+    val joao = Funcioario("João", 4000.0, "CLT")
+    val pedro = Funcioario("Pedro", 2000.0, "PJ")
+    val maria = Funcioario("Maria", 3000.0, "CLT")
+
+    /* make a list of funcionarios*/
+    val funcionarios = mutableListOf(joao, maria)
+    funcionarios.forEach { println(it) }
+
+    println("-----LIST--------")
+    /*add another funcionario at the list*/
+    funcionarios.add(pedro)
+    funcionarios.forEach { println(it) }
+
+    println("-------------")
+    /*remove a funcionario from the list*/
+    funcionarios.remove(joao)
+    funcionarios.forEach { println(it) }
+
+    println("------SET-------")
+    /*create a set of funcionarios*/
+    val funcionarioSet = mutableSetOf(joao)
+    funcionarioSet.forEach { println(it) }
+
+    println("------SET-------")
+    /*add elemento to the set*/
+    funcionarioSet.add(pedro)
+    funcionarioSet.add(maria)
+    funcionarioSet.forEach { println(it) }
+}
