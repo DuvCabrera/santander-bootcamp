10a7e9dbb368921d6f563b0ec79022c469e77b04
DuvCabrera <eduardoviniciuscabrera@gmail.com>
2021-07-03 12:45:15 -0300

    modified testlist

--- api-collections/src/main/kotlin/one/gititalinnovation/collections/TesteList.kt
+++ api-collections/src/main/kotlin/one/gititalinnovation/collections/TesteList.kt
@@ -2,7 +2,7 @@ package one.gititalinnovation.collections
 
 /*class for test listOf function */
 fun main() {
-    val joao = Funcioario("João", 1000.0)
+    val joao = Funcioario("João", 4000.0)
     val pedro = Funcioario("Pedro", 2000.0)
     val maria = Funcioario("Maria", 3000.0)
 
@@ -12,6 +12,10 @@ fun main() {
 
     println("---------------")
     println(funcionarios.find {it.nome == "Maria"})
+
+    /*sort the components in an array by a specific value*/
+    println("---------------")
+    funcionarios.sortedBy { it.salario }.forEach {println(it)}
 }
 
 /*funcionario class created and string appearance modified for better look*/
