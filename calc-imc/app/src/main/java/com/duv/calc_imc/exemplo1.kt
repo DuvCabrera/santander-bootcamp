eecd5a484905c2716dd8bdfc9741f97744925415
DuvCabrera <eduardoviniciuscabrera@gmail.com>
2021-07-08 16:53:50 -0300

    created and add new interface for the project, linear and relative layout

---
+++ calc-imc/app/src/main/java/com/duv/calc_imc/exemplo1.kt
@@ -0,0 +1,11 @@
+package com.duv.calc_imc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class exemplo1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exemplo1)
+    }
+}
---
+++ calc-imc/app/src/main/java/com/duv/calc_imc/relative.kt
@@ -0,0 +1,11 @@
+package com.duv.calc_imc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class relative : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_relative)
+    }
+}
