8b427a96873a2a4d36b304ea717966c4c4cb7b99
DuvCabrera <eduardoviniciuscabrera@gmail.com>
2021-07-08 22:59:42 -0300

    modified project interface

--- calc-imc/app/src/main/java/com/duv/calc_imc/MainActivity.kt
+++ calc-imc/app/src/main/java/com/duv/calc_imc/MainActivity.kt
@@ -1,42 +1,20 @@
 package com.duv.calc_imc
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-    override fun onStart() {
-        super.onStart()
-        Log.w("lifcycle", "entrei no START - deixei a tela visivel para você")
-    }
 
-    override fun onResume() {
-        super.onResume()
-        Log.w("lifcycle", "entrei no RESUME - agora você pode Interagir com a tela")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.w("lifcycle", "PAUSE - a tela perdeu o coco, você não pode mais Interagir")
-    }
-
-    override fun onStop() {
-        Log.w("lifcycle", "STOP - a tela não está mais visivel mas ainda existe")
-        super.onStop()
-    }
-
-    override fun onDestroy() {
-        Log.w("lifcycle", "\n entrei no DESTROY - oh não! a tela foi destruída")
-        super.onDestroy()
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Log.w("lifcycle", "\nentrei no RESTART - a tela esta retomando o foco")
 
     }
-}
+
+   }
+
+
+
