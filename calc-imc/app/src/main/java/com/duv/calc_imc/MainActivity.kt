d216888f934a81c214e4342d4fdc2b88dab418ea
DuvCabrera <eduardoviniciuscabrera@gmail.com>
2021-07-09 01:47:19 -0300

    mainactivity modified, listeners and functions add

--- calc-imc/app/src/main/java/com/duv/calc_imc/MainActivity.kt
+++ calc-imc/app/src/main/java/com/duv/calc_imc/MainActivity.kt
@@ -1,20 +1,47 @@
 package com.duv.calc_imc
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doAfterTextChanged
+import androidx.core.widget.doOnTextChanged
+import com.duv.calc_imc.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        setListeners()
     }
 
-   }
+    private fun setListeners() {
+        binding.alturaEDT?.doAfterTextChanged { text ->
+            Toast.makeText(this, text.toString(), Toast.LENGTH_SHORT).show()
+        }
+        binding.pesoEDT.doOnTextChanged{ text, _, _, _ ->
+            binding.titleTXT?.text = text
+        }
+        binding.calcBTN.setOnClickListener {
+            calculaIMC(binding.pesoEDT.text.toString(), binding.alturaEDT.text.toString())
+        }
+    }
+
+    private fun calculaIMC(peso: String, altura: String){
+        val peso = peso.toFloatOrNull()
+        val altura = altura.toFloatOrNull()
+
+        if (peso != null && altura != null) {
+            val imc = peso / (altura * altura)
+            binding.titleTXT.text = "O seu IMC é de {%.2f}".format(imc)
+        }
+    }
+}
 
 
 
