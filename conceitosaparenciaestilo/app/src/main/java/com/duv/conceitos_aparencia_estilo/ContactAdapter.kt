6a459e3dfc9d1582f1523299f336baafe180f3f5
DuvCabrera <eduardoviniciuscabrera@gmail.com>
2021-07-20 15:22:45 -0300

    modified contactAdapter, MainActivity, and respective xml

--- conceitosaparenciaestilo/app/src/main/java/com/duv/conceitos_aparencia_estilo/ContactAdapter.kt
+++ conceitosaparenciaestilo/app/src/main/java/com/duv/conceitos_aparencia_estilo/ContactAdapter.kt
@@ -3,6 +3,7 @@ package com.duv.conceitos_aparencia_estilo
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class ContactAdapter :RecyclerView.Adapter<ContactAdapter.ContactAdapterViewHolder>() {
@@ -22,9 +23,20 @@ class ContactAdapter :RecyclerView.Adapter<ContactAdapter.ContactAdapterViewHold
         return list.size
     }
 
-    class ContactAdapterViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
-        fun bind(contact: Contact) {
+    fun updateList(list: List<Contact>) {
+        this.list.clear()
+        this.list.addAll(list)
+        notifyDataSetChanged()
+    }
 
+    class ContactAdapterViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+        private val tvName: TextView = itemView.findViewById(R.id.tv_name)
+        private val tvPhone: TextView = itemView.findViewById(R.id.tv_phone)
+        private val ivPhotograph: TextView = itemView.findViewById(R.id.iv_photograph)
+
+        fun bind(contact: Contact) {
+            tvName.text = contact.name
+            tvPhone.text = contact.phone
         }
     }
 }
--- conceitosaparenciaestilo/app/src/main/java/com/duv/conceitos_aparencia_estilo/MainActivity.kt
+++ conceitosaparenciaestilo/app/src/main/java/com/duv/conceitos_aparencia_estilo/MainActivity.kt
@@ -2,10 +2,35 @@ package com.duv.conceitos_aparencia_estilo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private val rvList: RecyclerView by lazy {
+        findViewById<RecyclerView>(R.id.rv_list)
+    }
+    private val adapter = ContactAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bindViews()
+    }
+
+    private fun bindViews() {
+        rvList.adapter = adapter
+        rvList.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun updateList() {
+        adapter.updateList(
+            arrayListOf(
+                Contact(
+                    "igor Ferrani",
+                    "(00) 0000-0000",
+                    "img.png"
+                )
+            )
+        )
     }
 }
