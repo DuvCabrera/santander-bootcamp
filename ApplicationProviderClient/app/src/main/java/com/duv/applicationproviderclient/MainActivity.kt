d0e861078b1f3ea85cb97a2f7fbd1f4f56402f32
DuvCabrera <eduardoviniciuscabrera@gmail.com>
2021-08-05 18:22:56 -0300

    modified main

--- ApplicationProviderClient/app/src/main/java/com/duv/applicationproviderclient/MainActivity.kt
+++ ApplicationProviderClient/app/src/main/java/com/duv/applicationproviderclient/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.duv.applicationproviderclient
 
+import android.database.Cursor
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,8 +20,24 @@ class MainActivity : AppCompatActivity() {
 
         noteRefreshButton = findViewById(R.id.client_button_refresh)
         notesRecycler = findViewById(R.id.client_list)
+        getContentProvider()
 
-        noteRefreshButton.setOnClickListener { }
+        noteRefreshButton.setOnClickListener { getContentProvider() }
 
+    }
+
+    private fun getContentProvider() {
+        try {
+            val url = "content://com.duv.aplicationcontentprovider.provider/notes"
+            val data = Uri.parse(url)
+            val cursor: Cursor? =
+                contentResolver.query(data, null, null, null, "title")
+            notesRecycler.apply {
+                layoutManager = LinearLayoutManager(this@MainActivity)
+                adapter = ClientAdapter(cursor as Cursor)
+            }
+        } catch (ex: Exception) {
+            ex.printStackTrace()
+        }
     }
 }
